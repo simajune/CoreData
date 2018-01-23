c08511bc200424cdef84d13238243722ae89214c
simajune <tejayjung@gmail.com>
2018-01-23 12:37:28 +0900

    Generate  WeatherTejay Project

---
+++ WeatherTejay/WeatherTejay/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WeatherTejay
+//
+//  Created by SIMA on 2018. 1. 22..
+//  Copyright © 2018년 devtejay. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
