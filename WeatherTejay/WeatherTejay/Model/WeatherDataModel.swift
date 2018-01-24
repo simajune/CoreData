598e00b1ddd1b6826d083cee44ed88a1347de870
simajune <tejayjung@gmail.com>
2018-01-25 02:00:29 +0900

    Add Asset, DataModel

---
+++ WeatherTejay/WeatherTejay/Model/WeatherDataModel.swift
@@ -0,0 +1,29 @@
+
+import Foundation
+
+class WeatherDataModel {
+    var temperature: Int = 0
+    var condition: Int = 0
+    var city: String = ""
+    var weatherIconName: String = ""
+    
+    func updateWeatherIcon(condition: Int) -> String {
+        switch (condition) {
+        case 0..<300 :
+            return "Thunder"
+        case 300..<500 :
+            return "Drizzle"
+        case 500..<600 :
+            return "Rainy"
+        case 600..<700 :
+            return "Snowing"
+        case 700..<800 :
+            return "Windy"
+        case 800..<900 :
+            return "Cloudy"
+        default:
+            return "dunno"
+        }
+    }
+    
+}
--- WeatherTejay/WeatherTejay/ViewController/MainViewController.swift
+++ WeatherTejay/WeatherTejay/ViewController/MainViewController.swift
@@ -6,9 +6,14 @@ import SwiftyJSON
 
 class MainViewController: UIViewController, CLLocationManagerDelegate {
 
+    //Variable
     let locationManager = CLLocationManager()
+    let weatherDataModel = WeatherDataModel()
     
     @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var weatherIcon: UIImageView!
+    @IBOutlet weak var tempLabel: UILabel!
+    @IBOutlet weak var dustLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +23,44 @@ class MainViewController: UIViewController, CLLocationManagerDelegate {
         locationManager.requestWhenInUseAuthorization()
         locationManager.startUpdatingLocation()
     }
+    //MARK: - Networking
+    func getWeatherData(url: String, parameters: [String: String]) {
+        Alamofire.request(url, method: .get, parameters: parameters).responseJSON { response in
+            if response.result.isSuccess {
+                print("Success! Got the weather data")
+                let weatherJSON: JSON = JSON(response.result.value!)
+                print(weatherJSON)
+                self.updateWeatherData(json: weatherJSON)
+                
+            }else {
+                print("Error \(response.result.error!)")
+                self.locationLabel.text = "Connection Issues"
+            }
+        }
+    }
+    
+    //MARK: - JSON Parsing
+    func updateWeatherData(json: JSON) {
+        print(json["list"].count)
+        if let tempResult = json["list"][0]["main"]["temp"].double {
+            weatherDataModel.temperature = Int(tempResult - 273.15)
+            weatherDataModel.city = json["city"]["name"].stringValue
+            weatherDataModel.condition = json["list"][0]["weather"][0]["id"].intValue
+            weatherDataModel.weatherIconName = weatherDataModel.updateWeatherIcon(condition: weatherDataModel.condition)
+            
+            updateUIWithWeatherDate()
+        }else {
+            locationLabel.text = "Weather Unavailable"
+        }
+    }
+    
+    //MARK: - UI Updates
+    func updateUIWithWeatherDate() {
+        locationLabel.text = weatherDataModel.city
+        tempLabel.text = String(weatherDataModel.temperature)
+        weatherIcon.image = UIImage(named: weatherDataModel.weatherIconName)
+    }
+    
     
     //MARK: - Location Manager Delegate
     //didUpdateLocations
@@ -25,15 +68,16 @@ class MainViewController: UIViewController, CLLocationManagerDelegate {
         let location = locations[locations.count - 1]
         if location.horizontalAccuracy > 0 {
             locationManager.stopUpdatingLocation()
+            locationManager.delegate = nil
+            
             print("long = \(location.coordinate.longitude), lat = \(location.coordinate.latitude)")
             
             let longitude = String(location.coordinate.longitude)
             let latitude = String(location.coordinate.latitude)
             
-            let param: [String: String] = ["lat": latitude, "long": longitude, "appid": weatherAPIKey]
+            let param: [String: String] = ["lat": latitude, "lon": longitude, "appid": weatherAPIKey]
+            getWeatherData(url: weatherURL, parameters: param)
         }
-            
-        
     }
     
     //didFailWithError
