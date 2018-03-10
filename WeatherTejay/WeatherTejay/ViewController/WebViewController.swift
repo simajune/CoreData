73a1f08ab90973416fc2e17331703f8ab90e8a83
simajune <tejayjung@gmail.com>
2018-03-10 11:07:17 +0900

    웹뷰 조정중

--- WeatherTejay/WeatherTejay/ViewController/WebViewController.swift
+++ WeatherTejay/WeatherTejay/ViewController/WebViewController.swift
@@ -9,21 +9,31 @@ class WebViewController: UIViewController, WKUIDelegate {
     
     @IBOutlet weak var navibarView: UIView!
     @IBOutlet weak var containerView: UIView!
+    //Navigation
+    @IBOutlet weak var menuBtn: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         let webConfiduration = WKWebViewConfiguration()
-        
         webView = WKWebView(frame: .zero, configuration: webConfiduration)
         self.view.addSubview(webView)
         webView.snp.makeConstraints {
             $0.top.bottom.left.right.equalTo(containerView)
         }
-        loadURL()
+        //메뉴 버튼에 대한 메소드 설정
+        menuBtn.addTarget(self.revealViewController(), action: #selector(SWRevealViewController.revealToggle(_:)), for: .touchUpInside)
+        //매뉴를 보기 위한 조건들 추가 - 오른쪽으로 슬라이드
+        self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        self.view.addGestureRecognizer(self.revealViewController().tapGestureRecognizer())
+        
+        setupWebView()
     }
     
-    func loadURL() {
-        let myURL = URL(string: "https://earth.nullschool.net/#current/particulates/surface/level/overlay=pm10/orthographic=-231.37,36.93,1930/loc=126.593,38.140")
+    func setupWebView() {
+        
+        let myURL = URL(string: "https://earth.nullschool.net/")
+
+//        let myURL = URL(string: "https://earth.nullschool.net/#current/particulates/surface/level/overlay=pm2.5/orthographic=-233.74,38.18,2000/loc=125.820,38.707")
         let myRequest = URLRequest(url: myURL!)
         webView.load(myRequest)
     }
