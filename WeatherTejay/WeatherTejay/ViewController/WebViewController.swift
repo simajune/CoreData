2f60f7d3eb0d3aa74a8e9a2759e3efc4bb12b53c
simajune <tejayjung@gmail.com>
2018-03-12 14:29:07 +0900

    스케일 이미지 추가 및 이미지 뷰 추가, 스택 뷰 추가

--- WeatherTejay/WeatherTejay/ViewController/WebViewController.swift
+++ WeatherTejay/WeatherTejay/ViewController/WebViewController.swift
@@ -15,23 +15,50 @@ class WebViewController: UIViewController, WKUIDelegate {
     var pm10Btn: UIButton = {
         let btn = UIButton()
         btn.backgroundColor = .black
-        btn.layer.cornerRadius = 5
         btn.layer.borderWidth = 1
-        btn.setTitle("PM10", for: .normal)
+        btn.setTitle("미세먼지", for: .normal)
         btn.setTitleColor(.white, for: .normal)
+        btn.titleLabel?.font = UIFont(name: "Yanolja Yache OTF", size: 17)
         return btn
     }()
     var pm25Btn: UIButton = {
         let btn = UIButton()
         btn.backgroundColor = .black
-        btn.layer.cornerRadius = 5
         btn.layer.borderWidth = 1
-        btn.setTitle("PM2.5", for: .normal)
+        btn.setTitle("초미세먼지", for: .normal)
         btn.setTitleColor(.white, for: .normal)
+        btn.titleLabel?.font = UIFont(name: "Yanolja Yache OTF", size: 17)
         return btn
     }()
-    
-    
+    var coBtn: UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .black
+        btn.layer.borderWidth = 1
+        btn.setTitle("일산화탄소", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.titleLabel?.font = UIFont(name: "Yanolja Yache OTF", size: 17)
+        return btn
+    }()
+    var co2Btn: UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .black
+        btn.layer.borderWidth = 1
+        btn.setTitle("이산화탄소", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.titleLabel?.font = UIFont(name: "Yanolja Yache OTF", size: 17)
+        return btn
+    }()
+    var so2Btn: UIButton = {
+        let btn = UIButton()
+        btn.backgroundColor = .black
+        btn.layer.borderWidth = 1
+        btn.setTitle("아황산가스", for: .normal)
+        btn.setTitleColor(.white, for: .normal)
+        btn.titleLabel?.font = UIFont(name: "Yanolja Yache OTF", size: 17)
+        return btn
+    }()
+    var dustStackView: UIStackView!
+    var scaleImgView: UIImageView!
     @IBOutlet weak var navibarView: UIView!
     @IBOutlet weak var containerView: UIView!
     //Navigation
@@ -76,11 +103,10 @@ class WebViewController: UIViewController, WKUIDelegate {
         
         webView.snp.makeConstraints {
             $0.top.left.right.equalTo(containerView)
-//            $0.bottom.equalTo(containerView)
             $0.bottom.equalTo(containerView).multipliedBy(1.05)
         }
         
-        
+        //CoverView Setting
         webView.addSubview(coverView)
         coverView.snp.makeConstraints {
             $0.top.bottom.left.right.equalTo(containerView)
@@ -93,6 +119,30 @@ class WebViewController: UIViewController, WKUIDelegate {
             $0.centerY.equalTo(containerView).multipliedBy(1.7)
             $0.height.equalTo(50)
             $0.width.equalTo(containerView).multipliedBy(0.2)
+        }
+        
+        
+        
+        scaleImgView = UIImageView(frame: CGRect.zero)
+        coverView.addSubview(scaleImgView)
+        scaleImgView.image = UIImage(named: "PMScale")
+        scaleImgView.snp.makeConstraints {
+            $0.left.right.equalTo(containerView)
+            $0.bottom.equalTo(containerView).offset(-1)
+            $0.height.equalTo(20)
+        }
+        
+        //StackView Setting
+        dustStackView = UIStackView(frame: .zero)
+        dustStackView = UIStackView(arrangedSubviews: [pm10Btn, pm25Btn, coBtn, co2Btn, so2Btn])
+        dustStackView.alignment = .fill
+        dustStackView.distribution = .fillEqually
+        dustStackView.axis = .horizontal
+        coverView.addSubview(dustStackView)
+        dustStackView.snp.makeConstraints {
+            $0.left.right.equalTo(containerView)
+            $0.bottom.equalTo(scaleImgView).offset(-21)
+            $0.height.equalTo(40)
         }
     }
     
